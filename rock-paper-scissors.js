f4808f651399a2c50320a7f1e8c93cb7e528d65d
Jeremiah <jeremiahjacobson10@gmail.com>
2023-03-22 01:10:53 -0400

    made game up to 5 rounds

--- rock-paper-scissors.js
+++ rock-paper-scissors.js
@@ -1,72 +1,16 @@
-// Description of game:
-// write a program where a person plays against a computer in a game of
-// rock paper scissors
-
-// Pseudocode:
-// create a variable called choice for user input
-// create an array of 3 with the strings Rock, Paper, and Scissors
-
-// the program asks the user for input to pick between rock, paper, and scissors and stores it
-// in the variable choice - make 1, 2, 3 the input choices for rock, paper and scissors
-
-// create a function so that the computer randomly chooses a number between 1 and 3 with 1 being rock and 3 being scissors
-// and store it in a variable computerChoice
-
-// create a function that takes both choices and have conditional statements that depict each possible outcome and in each conditional return the winner
-// if they tie set an else statement at the end to redo the game
-//set a winner variable to false
-// set a while loop that continues running the function until a winner is apparent
-
 let choiceString = { 1: "Rock", 2: "Paper", 3: "Scissors" }; // table for choices from user
-// let choice = parseInt(
-//   prompt("Choose 1 for Rock, 2 for paper, and 3 for scissors.")
-// );
-// let computerChoice = getComputerChoice();
-// // let selectionOutputString = `Your selection is: ${choiceString[choice]}\nComputers selection is: ${choiceString[computerChoice]}\n`;
-// // let winner = false;
+
 let round = 0;
 let userScore = 0;
 let compScore = 0;
-// while (round < 5) {
-//   if (choice < 1 || choice > 3) {
-//     choice = parseInt(
-//       prompt(
-//         "Invalid choice, please choose again. Choose 1 for Rock, 2 for paper, and 3 for scissors."
-//       )
-//     );
-//     continue;
-//   }
+//main
+const btn1 = document.querySelector(".btn1");
+const btn2 = document.querySelector(".btn2");
+const btn3 = document.querySelector(".btn3");
 
-//   winner = getWinner(choice, computerChoice);
-
-//   if (winner == "user") {
-//     userScore += 1;
-//     round += 1;
-//     console.log(whosWinning(userScore, compScore));
-//   } else if (winner == "comp") {
-//     compScore += 1;
-//     round += 1;
-//     console.log(whosWinning(userScore, compScore));
-//   } else if (winner == "tie") {
-//     console.log(whosWinning(userScore, compScore));
-//   }
-//   if (round != 5) {
-//     choice = parseInt(
-//       prompt("Play again! Choose 1 for Rock, 2 for paper, and 3 for scissors.")
-//     );
-//     computerChoice = getComputerChoice();
-//   }
-// }
-
-// if (userScore < compScore) {
-//   console.log(
-//     `the winner is the opponent with a final score of ${compScore}:${userScore}`
-//   );
-// } else if (userScore > compScore) {
-//   console.log(
-//     `the winner is the opponent with a final score of ${userScore}:${compScore}`
-//   );
-// }
+btn1.addEventListener("click", btnClick);
+btn2.addEventListener("click", btnClick);
+btn3.addEventListener("click", btnClick);
 function btnClick(e) {
   computerChoice = getComputerChoice();
 
@@ -79,46 +23,70 @@ function btnClick(e) {
   }
 
   const winnerBox = document.querySelector(".winnerBox");
+  const scoreBox = document.querySelector(".scoreBox");
 
   const { winner, winnerText } = getWinner(choice, computerChoice);
   // console.log(winnerText);
   if (winner == "user") {
     userScore += 1;
     round += 1;
-    winnerBox.textContent = `${winnerText}
-    ${whosWinning(userScore, compScore)}`;
+    winnerBox.textContent = `${winnerText}`;
+    scoreBox.innerHTML = `<span class= "title">Score:</span><br><strong>You:</strong>${userScore}<br><strong>Computer:</strong>${compScore}`;
   } else if (winner == "comp") {
     compScore += 1;
     round += 1;
-    winnerBox.textContent = `${winnerText}
-    ${whosWinning(userScore, compScore)}`;
+    winnerBox.textContent = `${winnerText}`;
+    scoreBox.innerHTML = `<span class= "title">Score:</span><br><strong>You:</strong>${userScore}<br><strong>Computer:</strong>${compScore}`;
   } else if (winner == "tie") {
-    winnerBox.textContent = `${winnerText}
-    ${whosWinning(userScore, compScore)}`;
+    winnerBox.textContent = `${winnerText}`;
+    scoreBox.innerHTML = `<span class= "title">Score:</span><br><strong>You:</strong>${userScore}<br><strong>Computer:</strong>${compScore}`;
   }
-  // if (round != 5) {
-  //   choice = parseInt(
-  //     prompt("Play again! Choose 1 for Rock, 2 for paper, and 3 for scissors.")
-  //   );
-  // }
-}
-const btn1 = document.querySelector(".btn1");
-const btn2 = document.querySelector(".btn2");
-const btn3 = document.querySelector(".btn3");
-
-btn1.addEventListener("click", btnClick);
-btn2.addEventListener("click", btnClick);
-btn3.addEventListener("click", btnClick);
-
-function whosWinning(userScore, compScore) {
-  if (userScore < compScore) {
-    return `Your opponent is winning with a score of ${compScore} to your score of ${userScore} \n the score is now: ${compScore}:${userScore}`;
-  } else if (userScore > compScore) {
-    return `You are winning with a score of ${userScore} to the computer's ${compScore} \n the score is now: ${userScore}:${compScore}`;
-  } else {
-    return `The game is tied! \n the score is now: ${userScore}:${compScore}`;
+  console.log(round);
+  if (round == 5) {
+    //selectors
+    const resetBtn = document.createElement("button");
+    const body = document.querySelector("body");
+    const container = document.querySelector("container");
+    //remove ability to select
+    btn1.removeEventListener("click", btnClick);
+    btn2.removeEventListener("click", btnClick);
+    btn3.removeEventListener("click", btnClick);
+    //display winner
+    winnerBox.textContent = winnerOfGame(userScore, compScore);
+    //add reset button
+    resetBtn.classList.add("resetButton");
+    resetBtn.textContent = "Reset";
+    body.appendChild(resetBtn);
+    resetBtn.addEventListener("click", resetGame);
   }
 }
+
+function winnerOfGame(userScore, compScore) {
+  if (userScore > compScore) {
+    return `You won!! Congrats!! Click the reset button below to start a new game.`;
+  } else if (compScore > userScore) {
+    return `You lost :( Click the reset button below to start a new game.`;
+  }
+}
+function resetGame(e) {
+  const winnerBox = document.querySelector(".winnerBox");
+  const scoreBox = document.querySelector(".scoreBox");
+  userScore = 0;
+  compScore = 0;
+  round = 0;
+  winner = "";
+  winnerText = "";
+  winnerBox.textContent = "";
+  scoreBox.innerHTML = "";
+  //remove reset button
+  const parentElement = e.target.parentNode;
+  parentElement.removeChild(e.target);
+  //add selection ability again
+  btn1.addEventListener("click", btnClick);
+  btn2.addEventListener("click", btnClick);
+  btn3.addEventListener("click", btnClick);
+}
+
 function getComputerChoice() {
   let computerChoice = Math.floor(Math.random() * (4 - 1) + 1); //random integer between 1 (inclusive) and 3
   return computerChoice;
